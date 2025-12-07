09bd87f83cac20fbde3886bd9baa5223551cdfec
MrSa3dola <saad.2002.ms@gmail.com>
2025-12-07 21:29:52 +0200

    add arrays

---
+++ 08myarray/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main (){
+	fmt.Println("welcome to arrays\n")
+	var fruitList [4] string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Orange"
+	fruitList[3] = "Banana"
+	fmt.Println("fruits are", fruitList)
+
+	fmt.Println("fruits are", len(fruitList)) // reserved memory (fixed)
+
+	var vegList = [] string{"potato", "beans", "mashroom"}
+	fmt.Println("veg list", vegList)
+	fmt.Println("veg list", len(vegList))
+}
