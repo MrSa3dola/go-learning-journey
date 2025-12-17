a76dc5b409c246fb6125db66226bb4d1353045e2
MrSa3dola <saad.2002.ms@gmail.com>
2025-12-17 23:13:36 +0200

    POST request

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 	fmt.Println("web verb video - LCO")
-	url := "http://localhost:8000"
-	PerformGetRequest(url)
+	url := "http://localhost:8000/post"
+	// PerformGetRequest(url)
+	PerformPostJSONRequest(url)
 }
 
 func PerformGetRequest(url string) {
@@ -32,7 +33,25 @@ func PerformGetRequest(url string) {
 	// fmt.Println(content)
 	// fmt.Println(string(content))
 }
+func PerformPostJSONRequest(url string) {
+	// fake JSON payload
+	requestBody := strings.NewReader(`
+		{
+			"Name" : "Saad",
+			"Coursename" : "AI",
+			"Price" : 100
+		}
+	`)
+	// no ',' at the end of the requestBody, gives me error :"
+	response, err := http.Post(url, "application/json", requestBody)
+	checkNil(err)
+	
+	defer response.Body.Close()
 
+	content, err := io.ReadAll(response.Body)
+	checkNil(err)
+	fmt.Println("the content is:", string(content))
+}
 func checkNil(err error){
 	if err != nil{
 		panic(err)
